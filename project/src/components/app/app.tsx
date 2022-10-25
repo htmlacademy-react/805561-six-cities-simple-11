0aab2352f4acd7857960066a8d417f3cf1b20e01
brakozyawr <brakozyawr@yandex.ru>
2022-10-25 23:31:08 +0300

    Начинаем программировать - правки

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,19 +1,13 @@
 import Main from '../../pages/main/main';
-import Header from '../../components/header/header';
 import {Offer} from '../../types/types';
-
 
 type AppScreenProps = {
   offersCount: number;
   offers: Offer[];
 }
 
-function App({offers, offersCount}:AppScreenProps): JSX.Element {
-  return (
-    <>
-      <Main offers={offers} offersCount={offersCount} />
-    </>
-  );
-}
+const App = ({offers, offersCount}:AppScreenProps): JSX.Element => (
+  <Main offers={offers} offersCount={offersCount} />
+);
 
 export default App;

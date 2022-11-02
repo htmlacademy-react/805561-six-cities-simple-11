b9a1c4069aa9a1ee21e40eba30ec75f2333c4224
brakozyawr <brakozyawr@yandex.ru>
2022-11-02 19:24:53 +0300

    Добавлено перечисление, исправлено имя типа пропсов

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,30 +1,24 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
+import {AppRoute} from '../../const';
 import Main from '../../pages/main/main';
 import Login from '../../pages/login/login';
 import Room from '../../pages/room/room';
 import NotFound from '../../pages/not-found/not-found';
 import {Offer} from '../../types/types';
 
-type AppScreenProps = {
+type AppProps = {
   offersCount: number;
   offers: Offer[];
 }
 
-const App = ({offers, offersCount}:AppScreenProps): JSX.Element => (
+const App = ({offers, offersCount}:AppProps): JSX.Element => (
   <BrowserRouter>
     <Routes>
-      <Route path='/'>
+      <Route path={AppRoute.Root}>
         <Route index element={<Main offers={offers} offersCount={offersCount} />} />
-        <Route path='/login' element={<Login />} />
-        {//так как все-таки правильно? так?
-        }
-        <Route path='/offer/:id' element={<Room />} />
-        {//или так?
-        }
-        <Route path='/offer/'>
-          <Route path='/offer/:id' element={<Room />} />
-        </Route>
+        <Route path={AppRoute.Login} element={<Login />} />
+        <Route path={AppRoute.Room} element={<Room />} />
         <Route path='*' element={<NotFound />} />
       </Route>
     </Routes>

a8d1defecdf41846683bd0f6c31d5878926b8ca4
brakozyawr <brakozyawr@yandex.ru>
2022-12-13 14:13:07 +0300

    Почистила комментарии

--- project/src/const.ts
+++ project/src/const.ts
@@ -11,14 +11,12 @@ export enum Filter {
   TopRated = 'Top rated first'
 }
 
+export const FILTERS = Object.values(Filter);
 
 export const CITIES = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'];
 
-//export const FILTERS = ['Popular', 'Price: low to high', 'Price: high to low', 'Top rated first'];
-export const FILTERS = Object.values(Filter);
-
 export const INITITIAL_CITY = 'Paris';
+export const INITIAL_SORT = 'Popular';
 
 export const URL_MARKER_DEFAULT = '../img/pin.svg';
-
 export const URL_MARKER_CURRENT = '../img/pin-active.svg';
--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,20 +1,17 @@
 import {createReducer} from '@reduxjs/toolkit';
 import {selectionCity, sortByCity, selectionFilter, sortByFilter} from './action';
 import {OfferData} from '../mocks/offers_mocks';
-import {INITITIAL_CITY, Filter} from '../const';
+import {INITITIAL_CITY, Filter, INITIAL_SORT} from '../const';
 import {TOffer} from '../types/types';
 
-const INITIAL_SORT = 'Popular'; //перенести в константы
-const initialData = OfferData.filter((offer) => offer.city === INITITIAL_CITY );// по сути это  дуйствие filterByCity
-// eslint-disable-next-line no-console
-console.log(initialData);
+
+const initialData = OfferData.filter((offer) => offer.city === INITITIAL_CITY );
 
 const initialState = {
   city: INITITIAL_CITY,
   offerList: initialData,
   currentFilter: INITIAL_SORT,
 };
-
 
 const sort = function(offers: TOffer[], currentFilter: string): TOffer[] {
   const sortedOffers = offers.slice();
@@ -36,25 +33,18 @@ const reducer = createReducer(initialState, (builder) => {
   builder
     .addCase(selectionCity, (state, action) => {
       state.city = action.payload;
-      // eslint-disable-next-line no-console
-      //console.log(state.city);
     })
     .addCase(sortByCity, (state) => {
       state.offerList = OfferData.filter((offer) =>
         offer.city === state.city
       );
-      // eslint-disable-next-line no-console
-      //console.log(state.offerList);
     })
     .addCase(selectionFilter, (state, action) => {
       state.currentFilter = action.payload;
-      // eslint-disable-next-line no-console
-      console.log(state.currentFilter);
     })
     .addCase(sortByFilter, (state) => {
       state.offerList = sort(state.offerList, state.currentFilter);
     });
-
 });
 
 export {reducer};
